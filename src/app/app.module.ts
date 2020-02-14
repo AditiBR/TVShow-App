0b5df73d5bb06a3f9901218458d0698629dbc3aa
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-02-14 13:39:21 -0800

    Added a component display-shows, updated the port to 8080

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DisplayShowsComponent } from './display-shows/display-shows.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DisplayShowsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/display-shows/display-shows.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-display-shows',
+  templateUrl: './display-shows.component.html',
+  styleUrls: ['./display-shows.component.css']
+})
+export class DisplayShowsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
