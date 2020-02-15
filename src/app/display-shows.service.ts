900c4a4f2ec0d901fa034724bc24dd617d1e9308
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-02-15 08:57:32 -0800

    Call external API from service and added one more interface to map data from external service

--- src/app/display-shows.service.ts
+++ src/app/display-shows.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { IDisplayShowExternalSvcData } from './idisplay-show-external-svc-data';
+import { map } from 'rxjs/operators';
+import { IDisplayShows } from './idisplay-shows';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DisplayShowsService {
 
-  constructor(private httpClient: HttpClient) {
-    
+  constructor(private httpClient: HttpClient) {    
+   }
+
+   getShowDetails(showName: string) : Observable<IDisplayShows> {
+    return this.httpClient.get<IDisplayShowExternalSvcData>(`http://api.tvmaze.com/singlesearch/shows?q=${showName}`)
+    .pipe(map(data => this.transformDataToIDisplayShows(data)))
+
+   }
+   transformDataToIDisplayShows(iDisplayShowExternalSvcData: IDisplayShowExternalSvcData) : IDisplayShows{
+     return{
+      name: iDisplayShowExternalSvcData.name,
+      language: iDisplayShowExternalSvcData.language,
+      genres: iDisplayShowExternalSvcData.genres,
+      runtime: iDisplayShowExternalSvcData.runtime,
+      status: iDisplayShowExternalSvcData.status,
+      summary: iDisplayShowExternalSvcData.summary
+     }
    }
 }
---
+++ src/app/idisplay-show-external-svc-data.ts
@@ -0,0 +1,8 @@
+export interface IDisplayShowExternalSvcData {
+  name: string,
+  language: string,
+  genres: Array<string>,
+  status: string,
+  runtime: number,
+  summary: string
+}
