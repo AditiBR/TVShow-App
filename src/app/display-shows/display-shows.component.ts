6e8f368def1e6a5eab71acccadf952a6e21f5503
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-02-14 16:00:35 -0800

    Added interface and show hard hard coded data on web site

--- src/app/display-shows/display-shows.component.ts
+++ src/app/display-shows/display-shows.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IDisplayShows } from '../idisplay-shows';
 
 @Component({
   selector: 'app-display-shows',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DisplayShowsComponent implements OnInit {
 
-  constructor() { }
+  displayShows: IDisplayShows
+  constructor() {
+    this.displayShows ={
+      name:'showname',
+      language:'english'
+   } as IDisplayShows
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/idisplay-shows.ts
@@ -0,0 +1,8 @@
+export interface IDisplayShows {
+  name: string,
+  language: string,
+  genres: Array<string>,
+  runtime:number,
+  status:string,
+  summary:string
+}
