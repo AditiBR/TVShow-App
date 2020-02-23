177b6668f485d4cc8ef894d6c47f23b1f13a9ff4
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-02-23 15:35:26 -0800

    fixed search text box render issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,15 @@ import { DisplayShowsService } from './display-shows.service'
 import { HttpClientModule } from '@angular/common/http';
 import { SearchShowComponent } from './search-show/search-show.component'
 
-import { MatButtonModule} from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import 'hammerjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import 'hammerjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import{ FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
